673dcaa99d564738c6f80d0e2cf54e25842f2560
shy-Hozhi <shireetsosie@gmail.com>
2025-11-15 20:13:47 -0700

    Added Observer.java

--- SER315Implementation/src/Observer.java
+++ SER315Implementation/src/Observer.java
@@ -1,3 +1,20 @@
 public interface Observer {
+    /**
+    * Oberver Interface
+    * Author: Shiree T.
+    * Date: 11/15/2025
+    *
+    * Description:
+    * This is the interface that defines the Observer in the
+    * Design Pattern.
+    * Any class that implements this interface must define the
+    * sentStatus() method to recieve success or failure 
+    * notifications from the subject (RacerController).
+    *
+    * Notes:
+    * -sentStatus() is used to used to notify observers when
+    *  a registration attempt succeeds or fail. 
+    * -Concret class: SuccessObercer, FailObserver
+    **/
     void sentStatus(String message);
 }
