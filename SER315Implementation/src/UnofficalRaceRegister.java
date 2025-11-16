4ca0e6af9ba78d0816f1d38ea8685bf8550c2e35
cdaw2 <cdaw2@asu.edu>
2025-11-16 14:04:49 -0700

    implemented

--- SER315Implementation/src/UnofficalRaceRegister.java
+++ SER315Implementation/src/UnofficalRaceRegister.java
@@ -6,8 +6,12 @@ public class UnofficalRaceRegister implements RaceRegister {
         if (race.racers.contains(racer)) {
             return "Registration Failed: You are already registered for this race.";
         }
+        
         //check if registration is still open
-        //TODO: figure out how date is being stored
+        String deadline = race.getRegistrationDeadline();
+        if (date.compareTo(deadline) > 0) {
+            return "Registration Failed: Registration deadline has already passed";
+        }
        
         //register racer
         race.racers.add(racer);
