1609b53c00ce204f77250a8ed212984d9c3d947a
cdaw2 <cdaw2@asu.edu>
2025-11-16 01:22:57 -0700

    Update RaceRegister.java

--- SER315Implementation/src/RaceRegister.java
+++ SER315Implementation/src/RaceRegister.java
@@ -1,5 +1,3 @@
 public interface RaceRegister {
-
-    // Whoever is doing this one can I, Daniel, request that the register method returns a string saying successful or failed
-    // Please also have the register method have the signature public String register(int category, Race race, Racer racer)
+    String register(int category, Race race, Racer racer);
 }
