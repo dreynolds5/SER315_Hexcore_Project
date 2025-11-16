222d9cee0928743688635e631136a13ba12dc558
shy-Hozhi <shireetsosie@gmail.com>
2025-11-15 19:52:58 -0700

    Added SuccessObserver.java

--- SER315Implementation/src/SuccessObserver.java
+++ SER315Implementation/src/SuccessObserver.java
@@ -1,2 +1,17 @@
-public class SuccessObserver {
+public class SuccessObserver implements Observer {
+
+    /**
+    *SuccessObserver
+    *Author: Shiree T.
+    *Description:
+    *Concrete Oberver in the Observer Designn Pattern. 
+    *This class listens for status updates from the RacerController
+    *It reacts only to successful regiestration messages.
+    **/
+    @Override
+    public void sendStatus(String message){
+      if(message.toLowerCase().contains("success") ){
+        system.out.printLin("Success Observer: " + message );
+      }
+    }   
 }
