c9091036e8d6a6b8a5dadc8b322761bfa735c411
cdaw2 <cdaw2@asu.edu>
2025-11-16 01:47:54 -0700

    partially implemented

--- SER315Implementation/src/UnofficalRaceRegister.java
+++ SER315Implementation/src/UnofficalRaceRegister.java
@@ -1,2 +1,16 @@
-public class UnofficalRaceRegister {
+public class UnofficalRaceRegister implements RaceRegister {
+
+    @Override
+    public String register(int category, Race race, Racer racer) {
+        //check if they are already registered to the race
+        if (race.racers.contains(racer)) {
+            return "Registration Failed: You are already registered for this race.";
+        }
+        //check if registration is still open
+        //TODO: figure out how date is being stored
+       
+        //register racer
+        race.racers.add(racer);
+        return "Registration Successful";
+    }
 }
