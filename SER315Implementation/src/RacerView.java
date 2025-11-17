98066743161ad60027ef2c6b220c4b882033640a
oltabo <oltabozha@gmail.com>
2025-11-17 14:51:49 -0700

    Update RacerView.java

--- SER315Implementation/src/RacerView.java
+++ SER315Implementation/src/RacerView.java
@@ -2,37 +2,51 @@ import java.util.Observable;
 import java.util.Scanner;
 
 public class RacerView {
+    //the objects, connections to controller classes
     RacerController racerController = new RacerController();
     Observer successfulRegistration = new SuccessObserver();
     Observer failedRegistration =  new FailedObserver();
-
+    //main, create necessary objects
     public static void main(String[] args) {
         RacerView view = new RacerView();
         view.initObservers();
         view.basePage();
     }
-
+    //initialize observer
     public void initObservers(){
         this.racerController.addObserver(successfulRegistration);
         this.racerController.addObserver(failedRegistration);
     }
+
     public void basePage() {
         Scanner scanner = new Scanner(System.in);
         boolean loginStatus = false;
 
         while (true) {
-            System.out.println("Enter username to login:");
-            String username = scanner.nextLine();
+            System.out.println("1: log in");
+            System.out.println("2: exit");
+            System.out.println("Enter your choice\n");
+            int choice = scanner.nextInt();
 
-            System.out.println("Enter password to finish logging in:");
-            String password = scanner.nextLine();
+            switch(choice) {
+                case 1: //log in
+                    System.out.println("Enter username to login:");
+                    String username = scanner.nextLine();
 
-            loginStatus = racerController.login(username, password);
-            if (loginStatus) {
-                homePage(username);
-            } else {
-                System.out.println("Login Failed.");
+                    System.out.println("Enter password to finish logging in:");
+                    String password = scanner.nextLine();
+
+                    loginStatus = racerController.login(username, password);
+                    if (loginStatus) {
+                        homePage(username);
+                    } else {
+                        System.out.println("Login Failed.");
+                    }
+                    break;
+                case 2: //exit system
+                    System.exit(0);
             }
+
         }
     }
 
@@ -42,11 +56,11 @@ public class RacerView {
         int cat;
 
         while(inHomePage) {
-            System.out.println("Enter 1 to find races");
-            System.out.println("Enter 2 to register for race");
-            System.out.println("Enter 3 to view current category");
-            System.out.println("Enter 4 to log out");
-            System.out.println("Enter 5 to exit");
+            System.out.println("1: to find races");
+            System.out.println("2: to register for race");
+            System.out.println("4: to log out");
+            System.out.println("5: to exit");
+            System.out.println("Enter your choice\n");
             int choice = scanner.nextInt();
 
             switch (choice) {
@@ -54,21 +68,18 @@ public class RacerView {
                     System.out.println(racerController.showRaces());
                     break;
                 case 2: //register for a race
+                    System.out.println(racerController.showRaceSummary());
                     System.out.println("Enter raceID: ");
                     int raceID = scanner.nextInt();
                     System.out.println("Enter your current category: ");
-                    cat = scanner.nextInt();
+                    cat = racerController.getUserCategory(username);
                     racerController.raceRegister(username,raceID,cat);
                     break;
-                case 3:
-                    cat = racerController.getUserCategory(username);
-                    System.out.println("Current Category: " + cat);
-                    break;
-                case 4: //log out
+                case 3: //log out
                     System.out.println("logging out");
                     inHomePage = false;
                     break;
-                case 5: //exit system
+                case 4: //exit system
                     System.exit(0);
                     break;
                 default: //invalid choice
